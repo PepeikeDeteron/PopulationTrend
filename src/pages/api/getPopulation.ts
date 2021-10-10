bdc62a9d7762ef43ab2133e23785846e58cecc13
deteikepeperon@gmail.com <deteikepeperon@gmail.com>
2021-10-10 20:55:51 +0900

    各都道府県ごとの人口を取得する関数を追加

---
+++ src/pages/api/getPopulation.ts
@@ -0,0 +1,31 @@
+import axios from 'axios'
+import { Population, PopulationResponse } from '@/@types'
+
+export const getPopulation = async (
+  prefCode: number
+): Promise<Population[] | number[]> => {
+  const response = await axios.get<PopulationResponse>(
+    `https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode=${prefCode}`,
+    {
+      method: 'GET',
+      headers: {
+        'X-API-KEY': `${process.env.RESAS_API_KEY}`,
+      },
+    }
+  )
+
+  const data = response.data.result.data[0].data
+
+  // 提示されたワイヤーフレームを参考に、10年間隔 & 2020年度までのデータを取得する
+  const processingYears = data.filter((year) => {
+    return year.year % 10 === 0 && year.year <= 2020
+  })
+
+  // 人口のみ抽出する
+  // const populationData = processingYears.map((obj) => {
+  //   return obj.value
+  // })
+
+  console.log(processingYears)
+  return processingYears
+}
