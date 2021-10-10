fd2f5d33b40525cc24e3ecca3acd93c61e2f5bdb
deteikepeperon@gmail.com <deteikepeperon@gmail.com>
2021-10-10 21:05:22 +0900

    各都道府県ごとの人口を取得する関数のテストを追加

--- src/test/api.test.ts
+++ src/test/api.test.ts
@@ -1,9 +1,15 @@
 import '@jest/globals'
+import { getPopulation } from '@/pages/api/getPopulation'
 import { getPrefectures } from '@/pages/api/getPrefectures'
 
 describe('RESAS API 接続のテスト', () => {
   it('都道府県を取得できているか', async () => {
     const response = await getPrefectures()
     expect(response.length).toBe(47)
+  })
+
+  it('東京都の総人口を取得できているか (10年間隔, 2020年まで)', async () => {
+    const response = await getPopulation(13) // 13 ... 東京都の prefCode
+    expect(response.length).toBe(7)
   })
 })
