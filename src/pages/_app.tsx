247f35b27b449684f0105479e49adb1bb66598ca
deteikepeperon@gmail.com <deteikepeperon@gmail.com>
2021-10-13 20:50:38 +0900

    Head を index.tsx から _app.tsx に移動

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Head from 'next/head'
 import { AppProps } from 'next/app'
 import 'normalize.css'
 import '../styles/globals.css'
@@ -6,6 +7,33 @@ import '../styles/globals.css'
 const App: React.VFC<AppProps> = ({ Component, pageProps }: AppProps) => {
   return (
     <>
+      <Head>
+        <title>都道府県別 総人口推移グラフ</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="manifest"
+          href="https://population-trend.vercel.app/manifest.json"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <meta
+          property="description"
+          content="地域経済分析システム「RESAS-API」から取得した都道府県別の総人口推移グラフを表示するサイトです"
+        />
+        <meta property="og:title" content="都道府県別 総人口推移グラフ" />
+        <meta property="og:site_name" content="都道府県別 総人口推移グラフ" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://population-trend.vercel.app" />
+        <meta
+          property="og:description"
+          content="地域経済分析システム「RESAS-API」から取得した都道府県別の総人口推移グラフを表示するサイトです"
+        />
+        <meta
+          property="og:image"
+          content="https://population-trend.vercel.app/favicon.ico"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
       <Component {...pageProps} />
     </>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,9 @@
 import React from 'react'
-import Head from 'next/head'
 import Template from '@/pages/Home'
 
 const Home: React.VFC = () => {
   return (
     <>
-      <Head>
-        <title>都道府県別 総人口推移グラフ</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link
-          rel="manifest"
-          href="https://population-trend.vercel.app/manifest.json"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="theme-color" content="#000000" />
-        <meta
-          property="description"
-          content="地域経済分析システム「RESAS-API」から取得した都道府県別の総人口推移グラフを表示するサイトです"
-        />
-        <meta property="og:title" content="都道府県別 総人口推移グラフ" />
-        <meta property="og:site_name" content="都道府県別 総人口推移グラフ" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://population-trend.vercel.app" />
-        <meta
-          property="og:description"
-          content="地域経済分析システム「RESAS-API」から取得した都道府県別の総人口推移グラフを表示するサイトです"
-        />
-        <meta
-          property="og:image"
-          content="https://population-trend.vercel.app/favicon.ico"
-        />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head>
       <Template />
     </>
   )
