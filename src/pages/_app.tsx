28afb65f42a535be16d19d156741b9b0c877d777
Yoshiaki SASAKI <deteikepeperon@gmail.com>
2021-10-13 21:04:58 +0900

    Update: Lighthouse 対策を追加 (#21)
    * OGP を追加
    
    * robots.txt を追加
    
    * meta タグを追加
    
    * manifest.json を追加
    
    * Head を index.tsx から _app.tsx に移動
    
    * タイトルを変更

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,31 @@ const App: React.VFC<AppProps> = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Head>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <title>Population Trend</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="manifest"
+          href="https://population-trend.vercel.app/manifest.json"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <meta
+          property="description"
+          content="地域経済分析システム「RESAS-API」から取得した都道府県別の総人口推移グラフを表示するサイトです"
+        />
+        <meta property="og:title" content="Population Trend" />
+        <meta property="og:site_name" content="Population Trend" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://population-trend.vercel.app" />
+        <meta
+          property="og:description"
+          content="地域経済分析システム「RESAS-API」から取得した都道府県別の総人口推移グラフを表示するサイトです"
+        />
+        <meta
+          property="og:image"
+          content="https://population-trend.vercel.app/favicon.ico"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <Component {...pageProps} />
     </>
