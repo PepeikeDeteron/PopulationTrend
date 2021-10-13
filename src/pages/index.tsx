cea4e3f2a4ae756e14ef4ead8de81e4f25b171e6
deteikepeperon@gmail.com <deteikepeperon@gmail.com>
2021-10-13 19:34:21 +0900

    OGP・meta タグを追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,19 @@ const Home: React.VFC = () => {
       <Head>
         <title>都道府県別 総人口推移グラフ</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta
+          property="description"
+          content="地域経済分析システム「RESAS-API」から取得した都道府県別の総人口推移グラフを表示するサイトです"
+        />
+        <meta property="og:title" content="都道府県別 総人口推移グラフ" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://population-trend.vercel.app" />
+        <meta
+          property="og:description"
+          content="地域経済分析システム「RESAS-API」から取得した都道府県別の総人口推移グラフを表示するサイトです"
+        />
+        <meta property="og:image" content="/favicon.ico" />
+        <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <Template />
     </>
